12a8605b7d35e0fd16da1b2d7e595f8282d0f018
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2023-04-17 13:04:22 +0700

    v1.3.0
    OnlyGuest middleware

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 // HomeController
-Route::get('/', [HomeController::class, 'landing']);
+Route::get('/', [HomeController::class, 'landing'])->middleware('onlyGuest');
 
-Route::get('/register', [HomeController::class, 'register']);
-Route::post('/register', [HomeController::class, 'register_save']);
+Route::get('/register', [HomeController::class, 'register'])->middleware('onlyGuest');
+Route::post('/register', [HomeController::class, 'register_save'])->middleware('onlyGuest');
 
-Route::get('/login', [HomeController::class, 'login']);
-Route::post('/login', [HomeController::class, 'login_save']);
+Route::get('/login', [HomeController::class, 'login'])->middleware('onlyGuest');
+Route::post('/login', [HomeController::class, 'login_save'])->middleware('onlyGuest');
