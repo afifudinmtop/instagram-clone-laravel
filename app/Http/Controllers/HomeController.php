2f7508476522e669831582d96a304cc9c3e9af20
Afifudin Maarif <32411703+afifudinmtop@users.noreply.github.com>
2023-04-17 12:41:57 +0700

    v1.2.0
    register dan login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -14,5 +16,81 @@ class HomeController extends Controller
     public function register()
     {
         return view('home/register');
+    }
+
+    public function register_save(Request $request)
+    {
+        // validasi
+        $validated = $request->validate([
+            'username' => ['required', 'min:3', 'max:20'],
+            'password' => ['required','min:3', 'max:20'],
+            'first_name' => ['required','min:3', 'max:20'],
+            'last_name' => ['required','min:3', 'max:20']
+        ]);
+
+        // assign
+        $uuid = uniqid();
+        $username = $request->input('username');
+        $first_name = $request->input('first_name');
+        $last_name = $request->input('last_name');
+        $password = Hash::make($request->input('password'));
+
+        // insert to db
+        DB::table('user')->insert([
+            'uuid' => $uuid,
+            'username' => $username,
+            'password' => $password,
+            'first_name' => $first_name,
+            'last_name' => $last_name
+        ]);
+
+        // redirect with success message
+        return back()->with('success', 'created account successfully!');
+    }
+
+    public function login()
+    {
+        return view('home/login');
+    }
+
+    public function login_save(Request $request)
+    {
+        // validasi
+        $validated = $request->validate([
+            'username' => ['required', 'min:3', 'max:20'],
+            'password' => ['required','min:3', 'max:20']
+        ]);
+
+        // assign
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        // cek on db
+        $exist = DB::table('user')
+                ->where('username', '=', $username)
+                ->limit(1)
+                ->count();
+
+        // find on db
+        $user = DB::table('user')
+                ->where('username', '=', $username)
+                ->limit(1)
+                ->get();
+
+        // wrong username
+        if (!$exist) {
+            return back()->with('error', 'invalid username/password!');
+        }
+        
+        // wrong username
+        if (!Hash::check($password, $user[0]->password)) {
+            return back()->with('error', 'invalid username/password!');
+        }
+
+        // store data session
+        $request->session()->put('user_uuid', $user[0]->uuid);
+
+        // all good
+        return redirect('/feed');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-
+// HomeController
 Route::get('/', [HomeController::class, 'landing']);
+
 Route::get('/register', [HomeController::class, 'register']);
+Route::post('/register', [HomeController::class, 'register_save']);
+
+Route::get('/login', [HomeController::class, 'login']);
+Route::post('/login', [HomeController::class, 'login_save']);
